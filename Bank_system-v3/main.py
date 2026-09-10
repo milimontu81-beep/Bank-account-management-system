547c31d164029fffa12783269b10bf6e06061260
milimontu81-beep <milimontu81@gmail.com>
2026-09-10 06:02:32 +0530

    Update main.py

--- Bank_system-v3/main.py
+++ Bank_system-v3/main.py
@@ -60,7 +60,7 @@ def ACCOUNT(accountnumber):
 #create a loop to run the program until the user exits
   while True:
 #to handle invalid input
-   #try:
+   try:
 #take the menu user choice
     choice = int(input("-Enter your choice :"))
     print("------------------------------------")
@@ -183,4 +183,47 @@ while True:
    
     print("===CREATE ACCOUNT===")
 #take the user input for creating account
+    username=input("-ENTER YOUR USERNAME:")
+    accountnumber=input("-ENTER 10 DIGIT ACCOUNT NUMBER:")
+    
+    accountnumberlist=cursor.execute("""SELECT 1 FROM accounts WHERE accountnumber=?""",(accountnumber,)).fetchall()
+    if accountnumberlist:
+     print("-ACCOUNT NUMBER ALREADY EXISTS!")
+     print("------------------------------------")
+    elif len(str(accountnumber))!=10:
+     print("-INVALID ACCOUNT NUMBER, ENTER A 10 DIGIT NUMBER!")
+     print("------------------------------------")
+    elif accountnumber.isdigit()==False:
+     print("-INVALID ACCOUNT NUMBER, ENTER A 10 DIGIT NUMBER!")
+     print("------------------------------------")
+    else:
+     password=input("-ENTER 8 CHARACTER TO CREATE PASSWORD:")
+     if len(str(password))!=8:
+      print("-INVALID PASSWORD, ENTER AN 8 CHARACTER PASSWORD!")
+      print("------------------------------------")
+     else:
+      phonenumber=input("-ENTER YOUR PHONE NUMBER:")
+      phonenumberlist=cursor.execute("""SELECT 1 FROM accounts WHERE phonenumber=?""",(phonenumber,)).fetchall()
+      if len(phonenumber)!=10 or phonenumber.isdigit()==False:
+       print("-INVALID PHONE NUMBER, ENTER A 10 DIGIT NUMBER!")
+       print("-----------------------------------")
+      elif phonenumberlist:
+       print("-PHONE NUMBER ALREADY EXISTS!")
+       print("-----------------------------------") 
+      else:
+#create new table for each user for saving there transaction history 
+        createaccount()
+#exit from the application 
+  elif choice==3:
+    print("==THANKS==")
+    break
+  else:
+   print("-INVALID CHOICE, ENTER 1 OR 2!")
+   print("---------------------------------------")
+ except:
+  print("-INVALID INPUT, ENTER AN INTEGER!")
+  print("----------------------------------------")
+    
+    
+
  
